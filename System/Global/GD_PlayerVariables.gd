075529cd9ca9df84e349c093d93d003235d82e95
Mekkathon <mattanyu@gmail.com>
2023-05-04 23:47:20 +0700

    Character Select wip2

--- System/Global/GD_PlayerVariables.gd
+++ System/Global/GD_PlayerVariables.gd
@@ -4,3 +4,5 @@ extends Node
 var allLevelOrder = ["11","12","21","22","23","24"]
 var unlockedLevels = ["11"] #add more every prevent stage clears
 var charDataList = [null,null,null]
+var charLevel = {"PC1":1,"PC2":1,"PC3":1,"PC4":1,"PC5":1,"PC6":1}
+var id2name = {"PC1":"Naruto","PC2":"Sakura","PC3":"Sasuke","PC4":"Itachi","PC5":"Deidara","PC6":"Kisame"}
--- System/Menu/CharacterSelect/GD_CharacterSelect.gd
+++ System/Menu/CharacterSelect/GD_CharacterSelect.gd
@@ -19,12 +19,52 @@ func set_char_slot(slot_id):
 					button.select()
 				else:
 					button.used(i+1)
+	if PlayerVar.charDataList[cur_slot]:
+		get_stat(PlayerVar.charDataList[cur_slot].char_id)
 	
 func _on_back_pressed():
 	queue_free()
 
+func get_stat(char_id):
+	print(char_id)
+	var char_path = "res://Character/PlayerCharacter/" + char_id + "/S_" + char_id +".tscn"
+	#var json_path = "res://Character/PlayerCharacter/" + char_id + "/" + char_id.to_lower() + "_stat.json"
+	#var json_file = load(json_path).get_data()
+	#print(json_file)
+	var chr = load(char_path).instantiate()
+	var chr_level = PlayerVar.charLevel[char_id]
+	
+	$Label2.text = PlayerVar.id2name[char_id]
+	$Label3.text = "Level " + str(chr_level)
+	
+	chr.get_stat(chr_level)
+	#print(chr.stat.atk,chr.stat.def,chr.stat.speed,chr.stat.health)
+	
+	$StatDetails/Label.text =  "HP:       " + str(chr.stat.health)
+	$StatDetails/Label2.text = "ATK:     " + str(chr.stat.atk)
+	$StatDetails/Label3.text = "DEF:     " + str(chr.stat.def)
+	$StatDetails/Label4.text = "SPD:     " + str(chr.stat.speed)
+	
+	match char_id:
+		"PC1","PC2":
+			chr.apply_scale(Vector2(1.5,1.5))
+			chr.set_position(Vector2(800,400))
+		"PC3":
+			chr.apply_scale(Vector2(1.4,1.4))
+			chr.set_position(Vector2(800,400))
+		"PC4","PC5":
+			chr.apply_scale(Vector2(1.2,1.2))
+			chr.set_position(Vector2(800,420))
+		"PC6" :
+			chr.apply_scale(Vector2(1.1,1.1))
+			chr.set_position(Vector2(800,430))
+	for prv_chr in $CharSprite.get_children():
+		prv_chr.queue_free()
+	$CharSprite.add_child(chr)
+	
 func reset_except(char_id):
 	selected_char = char_id
+	get_stat(selected_char)
 	for button in $CharButtons.get_children():
 		if button.char_id != char_id:
 			button.reset()
