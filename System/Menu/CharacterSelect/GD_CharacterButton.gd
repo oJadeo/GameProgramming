ad89884fb1204004d0884d088f4e86bb7012517c
Mekkathon <mattanyu@gmail.com>
2023-05-04 22:10:52 +0700

    character select page wip

--- System/Menu/CharacterSelect/GD_CharacterButton.gd
+++ System/Menu/CharacterSelect/GD_CharacterButton.gd
@@ -1,14 +1,26 @@
-extends TextureButton
+extends ColorRect
 @export var default_icon:Texture2D
 @export var selected_icon:Texture2D
+@export var hover_icon:Texture2D
 @export var used_icon:Texture2D
 @export var char_id:String
 
+func _ready():
+	$TextureButton.set_texture_hover(hover_icon)
+	
 func reset():
-	set_texture_normal(default_icon)
+	$TextureButton.set_texture_normal(default_icon)
+	color = Color.html("#00000000")
+	$ColorRect2.visible = false
 
 func select():
-	set_texture_normal(selected_icon)
+	$TextureButton.set_texture_normal(selected_icon)
+	color = Color.html("#001aff")
+	$ColorRect2.visible = false
 
-func used():
-	set_texture_normal(used_icon)
+func used(pos:int):
+	$TextureButton.set_texture_normal(used_icon)
+	color = Color.html("#ff0000")
+	$ColorRect2.visible = true
+	$ColorRect2.color = Color.html("#ff0000")
+	$ColorRect2/Label.text = "P"+str(pos)
