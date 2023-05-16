406e739386c0e204a95a2058e91ce6f370e2dd68
oJadeo <jade.jiravate@gmail.com>
2023-05-16 16:46:18 +0700

    Almost

--- Level/GD_TestLevel.gd
+++ Level/GD_TestLevel.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	win_level()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
