cde8ab83592242ecb3b5e6465a67aa65dd11b5b2
Mekkathon <mattanyu@gmail.com>
2023-05-18 17:26:56 +0700

    clear board

--- System/LevelEnd/GD_DefeatOverlay.gd
+++ System/LevelEnd/GD_DefeatOverlay.gd
@@ -2,14 +2,15 @@ extends Control
 
 func _on_restart_level_pressed():
 	Board.clear_board()
-	
 	Util.change_scene("res://Level/S_TestLevel.tscn")
 	get_parent().get_parent().queue_free()
 
 func _on_reselect_characters_pressed():
+	Board.clear_board()
 	Util.change_scene("res://System/Menu/TeamSelect/GD_TeamSelect.tscn")
 	get_parent().get_parent().queue_free()
 
 func _on_main_menu_pressed():
+	Board.clear_board()
 	Util.change_scene("res://System/Menu/MainMenu/GD_MainMenu.tscn")
 	get_parent().get_parent().queue_free()
